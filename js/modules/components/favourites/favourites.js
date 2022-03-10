28863caab95b2a0e22b47a3cb6b09b18e9cdcb00
Pål Nicholas Bakken <nicholas@Pals-MacBook-Air.local>
2022-03-10 22:25:53 +0100

    added a bunch of more stuff

--- js/modules/components/favourites/favourites.js
+++ js/modules/components/favourites/favourites.js
@@ -1,18 +1,28 @@
-export async function favourites() {
+import buildDataList from "../common/buildDataList";
+import displayMessage from "../common/displayMessage.js";
+
+(async function favourites() {
     const data = await fetch(dataURL);
     const favourites = getFavourites();
     if (!favourites) {
+        displayMessage("warning-message", "You don't seem to have any favourites", ".data-list");
+    } else {
+         const newData = data.filter((item) => {
+            favourites.forEach((id) => {
+                if (item.id === parseInt(id)) {
+                    return true;
+                }
+            });
+        });
 
-    } else newData = data.filter((item) => {
-        favourites.forEach((id) => {
-            if (item.id === parseInt(id)) {
-                return true;
-            }
-        })
-    })
-}
+        buildDataList(newData, ".data-list");
+    }
 
-function getFavourites() {
+
+
+})();
+
+export function getFavourites() {
     const storedFavourites = getFromStorage("favourites");
     if (!storedFavourites) {
         return null
