ec47dc42b2475485a0405598c27745a1a466884e
Pål N. Bakken <p.bakken@gmail.com>
2022-03-12 11:05:05 +0100

    committing local changes

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@
  *  Fetch data-array from api, display 3 props for each item.
  *  Each item will have an icon that allows users to add or remove items from a list of favourites. 
  *  Create a separate page where the user's favourite items will be displayed. 
+ *  Create a "remove favourites" button that clears favourites list and re-renders html.
  *  Let admin user sign in and edit or update items,
  **/
 
 import { clearStorage } from "./modules/settings/storage.js";
 
-//Having so much trouble with storage that I made this handy button for testing
-document.querySelector("#storage-clear").onclick = clearStorage; 
+ document.querySelector(".storage-clear").onclick = clearStorage;
